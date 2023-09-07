bd1dbd3fc63136d94a20c6c6d902a79d8f2d7816
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-09-07 23:37:12 +0100

    timestamp and git repo url added

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -3,15 +3,20 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ApiService {
   task1(slackName: string, track: string) {
+    const timestamp = getCurrentTimestamp();
     return {
       slack_name: slackName ? slackName : 'slack_name not provided',
       current_day: 'Monday',
-      utc_time: '2023-08-21T15:04:05Z',
+      utc_time: timestamp,
       track: track ? track : 'track not provided',
       github_file_url:
-        'https://github.com/username/repo/blob/main/file_name.ext',
-      github_repo_url: 'https://github.com/username/repo',
+        'https://github.com/Akinyeleib/zuri-hng-internship/blob/main/file_name.ext',
+      github_repo_url: 'https://github.com/Akinyeleib/zuri-hng-internship',
       status_code: '200',
     };
   }
+}
+
+function getCurrentTimestamp(): string {
+  return new Date().toISOString();
 }
