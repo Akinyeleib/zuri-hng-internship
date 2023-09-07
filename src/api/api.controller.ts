08f76cca95e9dab630e8923409c4784cd78fc14e
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-09-07 23:18:37 +0100

    query params extracted from request

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { ApiService } from './api.service';
 
-@Controller('api')
-export class ApiController {}
+@Controller('api') // slack_name=example_name&track=backend
+export class ApiController {
+  constructor(private apiService: ApiService) {}
+  @Get()
+  task1(
+    @Query('slack_name') slackName: string,
+    @Query('track') track: string,
+  ): any {
+    return this.apiService.task1(slackName, track);
+  }
+}
