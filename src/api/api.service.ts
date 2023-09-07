25948651a7d05d03223b55fb45cc1aad44628cb4
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-09-08 00:09:26 +0100

    Default slack-name and track implemented

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -5,10 +5,10 @@ export class ApiService {
   task1(slackName: string, track: string) {
     const [timestamp, currentDayOfWeek] = getDateTime();
     return {
-      slack_name: slackName ? slackName : 'slack_name not provided',
+      slack_name: slackName ? slackName : 'Akinyele Ibrahim',
       current_day: currentDayOfWeek,
       utc_time: timestamp,
-      track: track ? track : 'track not provided',
+      track: track ? track : 'Backend',
       github_file_url:
         'https://github.com/Akinyeleib/zuri-hng-internship/blob/main/src/api/api.service.ts',
       github_repo_url: 'https://github.com/Akinyeleib/zuri-hng-internship',
