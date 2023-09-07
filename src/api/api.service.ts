25eeaf2f7dd59a408e482bd7a88e4b9ca179e0f0
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-09-07 23:21:22 +0100

    return value added

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ApiService {}
+export class ApiService {
+  task1(slackName: string, track: string) {
+    return {
+      slack_name: slackName ? slackName : 'slack_name not provided',
+      current_day: 'Monday',
+      utc_time: '2023-08-21T15:04:05Z',
+      track: track ? track : 'track not provided',
+      github_file_url:
+        'https://github.com/username/repo/blob/main/file_name.ext',
+      github_repo_url: 'https://github.com/username/repo',
+      status_code: '200',
+    };
+  }
+}
